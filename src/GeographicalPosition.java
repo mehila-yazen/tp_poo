1a6a22499a1f8f065f912adb25901fd0a52b2098
Yazen-zizou <108405235+Yazen-zizou@users.noreply.github.com>
2026-04-26 14:16:03 +0100

    ff

--- src/GeographicalPosition.java
+++ src/GeographicalPosition.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class GeographicalPosition {
+    public Position a;
+    public Position b;
+    public Position c;
+    public Position d;
+
+    public GeographicalPosition(Position a , Position b , Position c , Position d){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
 }
--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Position {
+    public double x ;
+    public double y ;
+    public Position(double x , double y){
+        this.x = x;
+        this.y = y;
+    }
 }

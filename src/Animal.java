881b8b24bdd332a2e06cf155f8281a29e34c1071
Hashem <oh_bouchaal@esi.dz>
2026-04-26 15:17:33 +0000

    treeset is now events in animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,19 +1,21 @@
+import java.util.TreeSet;
+
 public class Animal {
     public String species;
-    public int age ;
+    public int age;
     public int weight;
     public Health health;
-    public Event  events;
+    public TreeSet<Event> events;
 
+    public Animal(String species, int age, int weight, Health health) {
+        this.species = species;
+        this.age = age;
+        this.weight = weight;
+        this.health = health;
+        this.events = new TreeSet<>();
+    }
 
-public Animal(String species , int age , int weight , Health health, Event events ){
-    this.species = species;
-    this.age = age;
-    this.weight = weight;
-    this.health = health;
-    this.events = events
-
-}
-
-void main() {
+    public void addEvent(Event event) {
+        this.events.add(event);
+    }
 }

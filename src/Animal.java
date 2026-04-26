0a76b292d4361944b8d62d4d2df214e24e6823cc
Hashem <oh_bouchaal@esi.dz>
2026-04-26 14:52:34 +0000

    animal added

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    public String species;
+    public int age ;
+    public int weight;
+    public Health health;
+    public Event  events;
+
+
+public Animal(String species , int age , int weight , Health health, Event events ){
+    this.species = species;
+    this.age = age;
+    this.weight = weight;
+    this.health = health;
+    this.events = events
+
+}
+
+void main() {
+}
